0fe5da7b1b90e1ce5adc0d4f6fc01b4b7c13337c
Yakkujit <yakkujit@gmail.com>
2024-11-13 05:30:28 +0900

    invite code

--- src/hook/useUser.ts
+++ src/hook/useUser.ts
@@ -1,5 +1,19 @@
 import { useRef } from "react";
 
+function generateInviteCode(userName: string, userId: string): string {
+    const data = `${userName} + ${userId}`;
+    let hash = 0;
+    for (let i = 0; i < data.length; i++) {
+        const char = data.charCodeAt(i);
+        hash = (hash << 5) - hash + char;
+        hash |= 0; // Convert to 32-bit integer
+    }
+
+    // Convert the hash to a positive number and encode it in base36
+    const code = Math.abs(hash).toString(36).toUpperCase();
+    return code;
+}
+
 // Define the structure of the Telegram WebApp User
 interface TelegramUser {
     id: number;
@@ -40,12 +54,17 @@ export const useUser = () => {
 
             userName.current = initDataUnsafe.user.username;
             userId.current = initDataUnsafe.user.id;
-
+            const code = generateInviteCode(
+                userName.current,
+                userId.current.toString()
+            );
             console.log("userId:", initDataUnsafe.user.id);
+            return { userName, userId, code };
         } else {
-            console.log("Telegram WebApp not found or user data is unavailable");
+            console.log(
+                "Telegram WebApp not found or user data is unavailable"
+            );
+            return null;
         }
     }
-
-    return { userName, userId };
 };
