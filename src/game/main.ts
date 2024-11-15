062184da6ce896eb811ff94ba9fbe71eb0c1b853
Yakkujit <yakkujit@gmail.com>
2024-11-15 15:59:54 +0900

    change ratio2

--- src/game/main.ts
+++ src/game/main.ts
@@ -3,9 +3,7 @@ import { Game as MainGame } from "./scenes/Game";
 import { MainMenu } from "./scenes/MainMenu";
 import { AUTO, Game } from "phaser";
 import { Preloader } from "./scenes/Preloader";
-const aspectWidth = 10;
-const aspectHeight = 19;
-const aspectRatio = aspectWidth / aspectHeight;
+
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -27,7 +25,7 @@ const config: Phaser.Types.Core.GameConfig = {
     scale: {
         mode: Phaser.Scale.FIT, // Scale to fit the screen while maintaining aspect ratio
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: window.innerHeight * aspectRatio, // Calculate width based on screen height
+        width: window.innerWidth, // Calculate width based on screen height
         height: window.innerHeight,              // Set initial height as screen height
       },
     scene: [Boot, Preloader, MainMenu, MainGame],
