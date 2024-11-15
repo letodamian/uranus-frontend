313632249783db1c92717aa4f6267c9f0a4cf775
Yakkujit <yakkujit@gmail.com>
2024-11-16 02:10:04 +0900

    screen

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -39,7 +39,7 @@ export class MainMenu extends Scene {
             .image(centerX, centerY + 90, "ring")
             .setDepth(10);
         this.ground = this.add
-            .image(centerX, centerY * 2 - 90, "ground")
+            .image(centerX, centerY * 2 - 80, "ground")
             .setDepth(100);
 
         this.title = this.add
