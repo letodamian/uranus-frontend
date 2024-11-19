38be80052c2f4912a460a662d8c59cd5a4e3c691
Yakkujit <yakkujit@gmail.com>
2024-11-19 18:25:03 +0900

    social

--- src/components/SocialTask.tsx
+++ src/components/SocialTask.tsx
@@ -12,7 +12,7 @@ interface SocialButtonProps {
 const SocialButton = ({ label, link, content }: SocialButtonProps) => {
     const user = useUser();
     const {data, isLoading, refetch} = useUserStatus(user?.userId);
-    console.log(data,"ggg")
+    console.log(data.joinedChannel,"ggg")
     // Handle button click to navigate to the link
     const handleButtonClick = () => {
         window.open(link, "_blank", "noopener,noreferrer");
